f9bfac33518ef87b4cf3e1ec432797ac7f7d86e2
VinceBmmrt <vincebommert75@gmail.com>
2025-05-15 17:30:25 +0200

    feat: handle form data with @RequestParam and model, set JSP prefix/suffix - Used @RequestParam to retrieve form inputs - Populated data into the Model object for view rendering - Configured JSP prefix and suffix in application properties

--- src/main/java/com/telusko/SpringBootWeb1/HomeController.java
+++ src/main/java/com/telusko/SpringBootWeb1/HomeController.java
@@ -1,9 +1,9 @@
 package com.telusko.SpringBootWeb1;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -12,18 +12,17 @@ public class HomeController {
 
     public String home() {
         System.out.println("Home Method called");
-        return "index.jsp";
+        return "index";
     }
 
     @RequestMapping("add")
-    public String add(HttpServletRequest req, HttpSession session) {
-        int num1 = Integer.parseInt(req.getParameter("num1"));
-        int num2 = Integer.parseInt(req.getParameter("num2"));
+    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model) {
+
         int result = num1 + num2;
 
-        session.setAttribute("result", result);
+        model.addAttribute("result", result);
         System.out.println(result);
-        return "result.jsp";
+        return "result";
     }
 
 
