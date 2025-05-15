63f83ad4141f0683ba892cb2b8a54699e9875ebb
VinceBmmrt <vincebommert75@gmail.com>
2025-05-15 17:11:56 +0200

    feat: add servlet data handling and display on result page Implemented data acceptance in servlet style Added result page to display submitted data

--- src/main/java/com/telusko/SpringBootWeb1/HomeController.java
+++ src/main/java/com/telusko/SpringBootWeb1/HomeController.java
@@ -1,5 +1,7 @@
 package com.telusko.SpringBootWeb1;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,9 +9,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping("/")
+
     public String home() {
         System.out.println("Home Method called");
         return "index.jsp";
+    }
+
+    @RequestMapping("add")
+    public String add(HttpServletRequest req, HttpSession session) {
+        int num1 = Integer.parseInt(req.getParameter("num1"));
+        int num2 = Integer.parseInt(req.getParameter("num2"));
+        int result = num1 + num2;
+
+        session.setAttribute("result", result);
+        System.out.println(result);
+        return "result.jsp";
     }
 
 
